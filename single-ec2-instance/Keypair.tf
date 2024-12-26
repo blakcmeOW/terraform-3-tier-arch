bb37691052d7969cafe92b42921c6514af5448b3
blakcmeOW <martinbattulayan@gmail.com>
2024-12-26 17:29:33 +0800

    Directory for deployment of 3-tier architecture

--- single-ec2-instance/Keypair.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_key_pair" "dove-key" {
-  key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKcezH/3TulNfmqFvR9W9TCto8biniM3mqlpBAqkzksS STIC-Martin@STIC-Martin"
-}
-
-resource "aws_key_pair" "test-key" {
-  key_name   = "test-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN7fv5kmwBhp8zd62HESVybnvMYBiu11Oq6VeMkD7bqT STIC-Martin@STIC-Martin"
-}
