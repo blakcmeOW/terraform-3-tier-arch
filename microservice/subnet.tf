1e1876d4c1069e5c33c96083035f929b49a210e8
blakcmeOW <martinbattulayan@gmail.com>
2024-12-29 16:06:08 +0800

    fix: list for tags added, count of az & cidr block changed to 3, tags for private subnet changed

--- microservice/subnet.tf
+++ microservice/subnet.tf
@@ -1,28 +1,31 @@
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id 
-  cidr_block = var.cidr[count.index]
-  availability_zone =var.az[count.index] 
-  count = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.cidr[count.index]
+  availability_zone = var.az[count.index]
+  count             = 3
 
   tags = {
-    Name =  "public-sub"
-  } 
+    #Name = "public-sub"\
+    Name = element(["web_server", "app_server", "jenkins_server"], count.index)
+  }
 }
 
 resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id 
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "ap-southeast-1b" 
-  count = 2
+  vpc_id = aws_vpc.main.id
+  #cidr_block        = var.cidr_private[count.index]
+  #availability_zone = var.az[count.index]
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-southeast-1b"
+  #count = 1
 
   tags = {
-    Name =  "private-sub3"
-  } 
+    Name = "private-sub-db"
+  }
 }
 
 data "aws_subnets" "sid" {
   filter {
-    name = "vpc-id"
+    name   = "vpc-id"
     values = [aws_vpc.main.id]
   }
 
