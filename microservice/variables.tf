cec39fddce4a29ee5fbe8444cf15106dc82bf2fb
blakcmeOW <martinbattulayan@gmail.com>
2024-12-29 16:07:08 +0800

    fix: cidr variable listadded

--- microservice/variables.tf
+++ microservice/variables.tf
@@ -1,9 +1,9 @@
 variable "cidr" {
-  type = list
-  default = ["10.0.1.0/24","10.0.2.0/24"]
+  type    = list(any)
+  default = ["10.0.1.0/24", "10.0.2.0/24","10.0.4.0/24"]
 }
 
 variable "az" {
-  type = list
-  default = ["ap-southeast-1a","ap-southeast-1b"]
+  type    = list(any)
+  default = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
 }
