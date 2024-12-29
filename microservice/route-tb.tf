da268ed377db7e76d213ace9170df6e6c6d8563b
blakcmeOW <martinbattulayan@gmail.com>
2024-12-29 16:01:10 +0800

    fix: variable name change from aws_internet_gateway > gateway_id

--- microservice/route-tb.tf
+++ microservice/route-tb.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table" "r-tb" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
-    aws_internet_gateway = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
@@ -12,20 +12,25 @@ resource "aws_route_table" "r-tb" {
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id = aws_subnet.public[count.index].index
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.r-tb.id
-  count = 2
+  count          = 2
 }
-
+/*
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+*/
 resource "aws_default_route_table" "def_r-tb" {
-    default_route_table_id = aws_vpc.main.default_route_table_id
+  default_route_table_id = aws_vpc.main.default_route_table_id
 
-    route = {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.natgw.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
 
-    tags = {
-      Name = "def_r-tb"
-    }
+  tags = {
+    Name = "def_r-tb"
+  }
 }
