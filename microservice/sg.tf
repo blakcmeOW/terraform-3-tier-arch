df47b3179ecdbef79eb21849676cfde25fea4359
blakcmeOW <martinbattulayan@gmail.com>
2024-12-29 16:03:21 +0800

    fix: remove = to each ingress and egress dictionary, to_port for aws rds added

--- microservice/sg.tf
+++ microservice/sg.tf
@@ -1,29 +1,29 @@
 resource "aws_security_group" "allow_tls" {
-  name = "allow_tls"
+  name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
-  ingress = {
+  ingress {
     description = "TLS from VPC"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  /*  
-  ingress = {
+
+  ingress {
     description = "TLS from VPC"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  */  
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -32,31 +32,38 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_security_group" "allow_tls_db" {
-  name = "allow_tls_db"
+  name        = "allow_tls_db"
   description = "Allow TLS inbound traffic"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
-  ingress = {
+  ingress {
     description = "TLS from VPC"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  /*
-  ingress = {
+
+  ingress {
     description = "TLS from VPC"
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  */
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
